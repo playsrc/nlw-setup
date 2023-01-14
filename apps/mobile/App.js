ed0647806d2647b72a966a663524eafc8d6b2b83
Mateus Abelli <mateusabelli@gmail.com>
2023-01-13 22:16:59 -0300

    feat(mobile): simple fetch with .env integration

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,7 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { API_URL } from "@env";
 
 export default function App() {
+  const [data, setData] = useState(null);
+
+  console.log(data);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
