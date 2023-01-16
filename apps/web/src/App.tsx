a8ce185ec08589953eea79fe7e729781c66d911c
Mateus Abelli <mateusabelli@gmail.com>
2023-01-16 20:42:55 -0300

    Add Web setup with Tailwind

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Habit } from "./components/Habit";
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={10} />
+      <Habit completed={20} />
+      <Habit completed={30} />
+    </div>
+  );
+}
+
+export default App;
