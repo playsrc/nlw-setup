4020e75d8f86b102d347a092279c36eea020e9da
Mateus Abelli <mateusabelli@gmail.com>
2023-01-17 20:21:50 -0300

    Update server file structure

---
+++ apps/server/src/lib/routes.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "./prisma";
+
+export async function appRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const habits = await prisma.habit.findMany();
+
+    return habits;
+  });
+}
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,17 +1,11 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./lib/routes";
 
 const app = fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/", async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app.listen({ port: 3333 }).then(() => {
   console.log(`HTTP Server running on http://localhost:3333`);
