156a4a856adf1e6d1e54fcfb1c260069cc07babb
Mateus Abelli <mateusabelli@gmail.com>
2023-01-18 21:27:19 -0300

    Add back button

--- apps/mobile/src/screens/New.tsx
+++ apps/mobile/src/screens/New.tsx
@@ -1,5 +1,12 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
+import { BackButton } from "../components/BackButton";
 
 export function New() {
-  return <View className="flex-1 bg-background px-8 pt-16"></View>;
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
+      </ScrollView>
+    </View>
+  );
 }
