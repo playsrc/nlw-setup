42be789e0473ad3e26ec228cfb535ea79cf1520d
Mateus Abelli <mateusabelli@gmail.com>
2023-01-19 21:45:56 -0300

    Fix API connection

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -7,6 +7,6 @@ const app = fastify();
 app.register(cors);
 app.register(appRoutes);
 
-app.listen({ port: 3333 }).then(() => {
-  console.log(`HTTP Server running on http://localhost:3333`);
+app.listen({ port: 3001, host: "10.0.0.104" }).then(() => {
+  console.log(`HTTP Server running on http://localhost:3001`);
 });
