1bc7d3c750540679b825dffe0f5f594104176a58
Mateus Abelli <mateusabelli@gmail.com>
2023-01-27 21:24:25 -0300

    Add mobile notifications with expo-notifications

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,6 +1,6 @@
 import "./src/lib/dayjs";
 
-import { StatusBar } from "react-native";
+import { StatusBar, Button } from "react-native";
 import {
   useFonts,
   Inter_400Regular,
@@ -8,9 +8,36 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
+import * as Notifications from "expo-notifications";
+
+async function scheduleNotification() {
+  const trigger = new Date(Date.now());
+  trigger.setMinutes(trigger.getMinutes() + 1);
+
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Olá, Mateus! ❤",
+      body: "Você praticou seus hábitos hoje?",
+    },
+    trigger,
+  });
+}
+
+async function getScheduleNotifications() {
+  const schedules = await Notifications.getAllScheduledNotificationsAsync();
+  console.log(schedules);
+}
 
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,6 +53,8 @@ export default function App() {
 
   return (
     <>
+      {/* <Button title="Enviar" onPress={scheduleNotification} />
+      <Button title="Agendadas" onPress={getScheduleNotifications} /> */}
       <Routes />
       <StatusBar
         barStyle="light-content"
