520fc0fe8ce4bf53cbe27a2d89fdcde9625951f8
Mateus Abelli <mateusabelli@gmail.com>
2023-01-27 21:23:38 -0300

    Add server routes for notifications with web-push

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,11 +1,13 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import { appRoutes } from "./lib/routes";
+import { notificationRoutes } from "./lib/notifications-routes";
 
 const app = fastify();
 
 app.register(cors);
 app.register(appRoutes);
+app.register(notificationRoutes);
 
 app.listen({ port: 3001, host: "10.0.0.104" }).then(() => {
   console.log(`HTTP Server running on http://localhost:3001`);
