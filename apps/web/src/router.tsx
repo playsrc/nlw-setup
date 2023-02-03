cfec45b53f79207d42db72ee64ca94480be37c1e
Mateus Abelli <mateusabelli@gmail.com>
2023-02-02 21:39:33 -0300

    Add protected route component

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,8 +1,8 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { trpc } from "./utils/trpc";
 
 import Home from "./pages/Home";
@@ -11,16 +11,28 @@ import App from "./pages/App";
 import Register from "./pages/Register";
 
 export default function Router() {
+  const token = localStorage.getItem("token");
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
           url: "http://localhost:3001/api",
+          headers() {
+            if (token) {
+              return {
+                Authorization: token,
+              };
+            }
+            return {};
+          },
         }),
       ],
     })
   );
+
+  // const hasAuth = trpc.users.me.useQuery();
+  // console.log(hasAuth);
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
@@ -30,11 +42,27 @@ export default function Router() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/app" element={<App />} />
+            <Route
+              path="/app"
+              element={
+                <ProtectedRoute>
+                  <App />
+                </ProtectedRoute>
+              }
+            />
             <Route path="*" element={<h1>404 - Página inexistente.</h1>} />
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
     </trpc.Provider>
   );
+}
+
+function ProtectedRoute({ children }: { children: ReactNode }) {
+  const hasAuth = trpc.users.me.useQuery().isSuccess;
+  console.log(hasAuth);
+  if (!hasAuth) {
+    return <Navigate to="/login" replace />;
+  }
+  return <>{children}</>;
 }
